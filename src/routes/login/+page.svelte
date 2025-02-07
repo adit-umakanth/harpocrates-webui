33f0b61086978c346858701615555a775592213f
Adit <aditya.umakanth@gmail.com>
2025-02-06 19:44:08 -0600

    added basic firebase auth

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import firebaseApp from '$lib/firebase';
+	import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+	const auth = getAuth(firebaseApp);
+	const provider = new GoogleAuthProvider();
+
+	function initiateSignIn() {
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				const user = result.user;
+				console.log(user);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+	function backToMain() {
+		goto('/');
+	}
+</script>
+
+<p>Login</p>
+<button onclick={initiateSignIn}>Google</button>
+<button onclick={backToMain}>Back to main</button>
