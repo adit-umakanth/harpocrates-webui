28b1bc079b8b3095d1f62b017507d40530cb2982
Adit <aditya.umakanth@gmail.com>
2025-02-16 20:55:06 -0600

    added logic for key derivation from password

--- src/lib/keyState.svelte.ts
+++ src/lib/keyState.svelte.ts
@@ -1,8 +1,19 @@
+import { dbState } from "./dbState.svelte";
+
 function createKeyState() {
     let derivedKey = $state();
-    function deriveKey(password: string) {
-        console.log("Key is derived")
-        derivedKey = password
+    async function deriveKey(password: string) {
+        const encodedPassword = new TextEncoder().encode(password)
+        const importedKey = await window.crypto.subtle.importKey("raw", encodedPassword, "PBKDF2", false, ['deriveKey'])
+        const salt = dbState.userInfoDoc?.salt!;
+        const saltBuffer = Uint8Array.from(atob(salt), c => c.charCodeAt(0));
+        derivedKey = await window.crypto.subtle.deriveKey(
+            { name: "PBKDF2", salt: saltBuffer, iterations: 1000000, hash: "SHA-256" },
+            importedKey,
+            { name: "AES-GCM", length: 256 },
+            false,
+            ['encrypt', 'decrypt']
+        )
     }
     return {
         get derivedKey() {
