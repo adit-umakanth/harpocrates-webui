62bbb850cf127a3e13df48596ab8d8d1e25d0191
Adit <aditya.umakanth@gmail.com>
2025-02-18 19:14:49 -0600

    ran linter to fix file formats

--- src/lib/dbState.svelte.ts
+++ src/lib/dbState.svelte.ts
@@ -1,33 +1,33 @@
-import { doc, getFirestore, onSnapshot, updateDoc } from "firebase/firestore";
-import firebaseApp from "./firebase";
-import { userState } from "./userState.svelte";
+import { doc, getFirestore, onSnapshot, updateDoc } from 'firebase/firestore';
+import firebaseApp from './firebase';
+import { userState } from './userState.svelte';
 
 type UserInfo = {
-    journals?: [string];
-    salt?: string;
-}
+	journals?: [string];
+	salt?: string;
+};
 
 function createDbState() {
-    let userInfoDoc = $state<UserInfo | null>(null);
-    let journalEntryDocs = $state();
-    let db = getFirestore(firebaseApp);
-    let userInfoDocReference = doc(db, 'users', userState.user!.uid)
+	let userInfoDoc = $state<UserInfo | null>(null);
+	let journalEntryDocs = $state();
+	let db = getFirestore(firebaseApp);
+	let userInfoDocReference = doc(db, 'users', userState.user!.uid);
 
-    onSnapshot(userInfoDocReference, async (newDoc) => {
-        userInfoDoc = newDoc.data() as UserInfo;
-        if (!("salt" in userInfoDoc)) {
-            let saltBuffer = window.crypto.getRandomValues(new Uint8Array(32))
-            await updateDoc(userInfoDocReference, {
-                salt: btoa(String.fromCharCode(...saltBuffer))
-            })
-        }
-    })
-    return {
-        get userInfoDoc() {
-            return userInfoDoc;
-        },
-        userInfoDocReference
-    }
+	onSnapshot(userInfoDocReference, async (newDoc) => {
+		userInfoDoc = newDoc.data() as UserInfo;
+		if (!('salt' in userInfoDoc)) {
+			let saltBuffer = window.crypto.getRandomValues(new Uint8Array(32));
+			await updateDoc(userInfoDocReference, {
+				salt: btoa(String.fromCharCode(...saltBuffer))
+			});
+		}
+	});
+	return {
+		get userInfoDoc() {
+			return userInfoDoc;
+		},
+		userInfoDocReference
+	};
 }
 
 export const dbState = createDbState();
--- src/lib/keyState.svelte.ts
+++ src/lib/keyState.svelte.ts
@@ -1,26 +1,32 @@
-import { dbState } from "./dbState.svelte";
+import { dbState } from './dbState.svelte';
 
 function createKeyState() {
-    let derivedKey = $state<CryptoKey | null>();
-    async function deriveKey(password: string) {
-        const encodedPassword = new TextEncoder().encode(password)
-        const importedKey = await window.crypto.subtle.importKey("raw", encodedPassword, "PBKDF2", false, ['deriveKey'])
-        const salt = dbState.userInfoDoc?.salt!;
-        const saltBuffer = Uint8Array.from(atob(salt), c => c.charCodeAt(0));
-        derivedKey = await window.crypto.subtle.deriveKey(
-            { name: "PBKDF2", salt: saltBuffer, iterations: 1000000, hash: "SHA-256" },
-            importedKey,
-            { name: "AES-GCM", length: 256 },
-            false,
-            ['encrypt', 'decrypt']
-        )
-    }
-    return {
-        get derivedKey() {
-            return derivedKey;
-        },
-        deriveKey
-    }
+	let derivedKey = $state<CryptoKey | null>();
+	async function deriveKey(password: string) {
+		const encodedPassword = new TextEncoder().encode(password);
+		const importedKey = await window.crypto.subtle.importKey(
+			'raw',
+			encodedPassword,
+			'PBKDF2',
+			false,
+			['deriveKey']
+		);
+		const salt = dbState.userInfoDoc?.salt!;
+		const saltBuffer = Uint8Array.from(atob(salt), (c) => c.charCodeAt(0));
+		derivedKey = await window.crypto.subtle.deriveKey(
+			{ name: 'PBKDF2', salt: saltBuffer, iterations: 1000000, hash: 'SHA-256' },
+			importedKey,
+			{ name: 'AES-GCM', length: 256 },
+			false,
+			['encrypt', 'decrypt']
+		);
+	}
+	return {
+		get derivedKey() {
+			return derivedKey;
+		},
+		deriveKey
+	};
 }
 
 export const keyState = createKeyState();
