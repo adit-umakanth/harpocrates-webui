bed49ff2f52e17c8fd47d08dc95391a483f017c8
Adit <aditya.umakanth@gmail.com>
2025-03-21 16:24:30 -0700

    added sign in with username and email

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,10 +2,19 @@
 	import { goto } from '$app/navigation';
 	import firebaseApp from '$lib/firebase';
 	import { userState } from '$lib/userState.svelte';
-	import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+	import {
+		createUserWithEmailAndPassword,
+		getAuth,
+		GoogleAuthProvider,
+		signInWithEmailAndPassword,
+		signInWithPopup
+	} from 'firebase/auth';
 
 	const auth = getAuth(firebaseApp);
 	const provider = new GoogleAuthProvider();
+
+	let manualEmail = $state<string>('');
+	let manualPassword = $state<string>('');
 
 	function initiateSignIn() {
 		signInWithPopup(auth, provider)
@@ -17,6 +26,29 @@
 				console.error(error);
 			});
 	}
+
+	function initiateSignInWithEmail() {
+		signInWithEmailAndPassword(auth, manualEmail, manualPassword)
+			.then((result) => {
+				const user = result.user;
+				console.log(user);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
+	function createUser() {
+		createUserWithEmailAndPassword(auth, manualEmail, manualPassword)
+			.then((result) => {
+				const user = result.user;
+				console.log(user);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
 	function backToMain() {
 		goto('/');
 	}
@@ -31,3 +63,12 @@
 <p>Login</p>
 <button class="btn" onclick={initiateSignIn}>Google</button>
 <button class="btn" onclick={backToMain}>Back to main</button>
+<br />
+<hr />
+<br />
+<div class="m-auto flex w-screen flex-row flex-wrap gap-3 p-2 md:size-1/4">
+	<input class="basis-full" type="text" placeholder="Email" bind:value={manualEmail} />
+	<input class="basis-full" type="password" placeholder="Password" bind:value={manualPassword} />
+	<button class="btn flex-1" onclick={createUser}>Register</button>
+	<button class="btn flex-1" onclick={initiateSignInWithEmail}>Sign in</button>
+</div>
